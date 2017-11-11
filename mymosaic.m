590cb28daa703cb88cac2a9ce5d87bb4f641abb1
Tonto <owusua@seas.upenn.edu>
2017-11-11 09:29:37 -0500

    Stitching almost done.

--- mymosaic.m
+++ mymosaic.m
@@ -3,9 +3,9 @@
 % Date created:
 
 function [img_mosaic] = mymosaic(img_input)
-% (INPUT) img_input: M ?N cell where M is the total number of frames in the 
+% (INPUT) img_input: M ?N cell where M is the total number of frames in the
 % video and N is three if the number of input videos is 3
-% (OUTPUT) img_mosaic: M ?1 cell vector representing the stitched image 
+% (OUTPUT) img_mosaic: M ?1 cell vector representing the stitched image
 % mosaic for every frame
 
 MAX_PTS = 100;
@@ -47,11 +47,15 @@ for i = 1:m
             end
             idx = m(m ~= -1);
             p2 = [x2(idx) y2(idx)];
-            [H{j-1}, inlier_ind] = ransac_est_homography(p1(:,1),p1(:,2),p2(:,1),p2(:,2), 1);
+            if (j > ceil(n/2))
+                [H{j-1}, inlier_ind] = ransac_est_homography(p2(:,1),p2(:,2),p1(:,1),p1(:,2), 1);
+            else
+                [H{j-1}, inlier_ind] = ransac_est_homography(p1(:,1),p1(:,2),p2(:,1),p2(:,2), 1);
+            end
             H{j - 1} = H{j-1} / H{j-1}(3,3);
         end
     end
-
+    
     
 end
 
@@ -59,20 +63,26 @@ end
 imageSize = size(im{1});
 
 % Create a cell to store the transform between image 1 and image i
-tform{1} = projective2d(eye(3));
+tform{ceil(n/2)} = projective2d(eye(3));
+[xlim(ceil(n/2),:), ylim(ceil(n/2),:)] = outputLimits(tform{ceil(n/2)}, [1 imageSize(2)], [1 imageSize(1)]);
 
 % Fill out the cell array with the transforms and determine the extents of
 % the panorama
 for i=1:size(H,1)
-    tform{i+1} = projective2d(H{i}');
-    [xlim(i,:), ylim(i,:)] = outputLimits(tform{i+1}, [1 imageSize(2)], [1 imageSize(1)]);
+    if i >= ceil(n/2)
+        tform{i+1} = projective2d(H{i}');
+        [xlim(i,:), ylim(i,:)] = outputLimits(tform{i+1}, [1 imageSize(2)], [1 imageSize(1)]);
+        continue;
+    end
+    tform{i} = projective2d(H{i}');
+    [xlim(i,:), ylim(i,:)] = outputLimits(tform{i}, [1 imageSize(2)], [1 imageSize(1)]);
 end
 
 xmin = min([1; xlim(:)]);
 ymin = min([1; ylim(:)]);
 xmax = max([imageSize(2); xlim(:)]);
 ymax = max([imageSize(1); ylim(:)]);
-    
+
 width = round(xmax - xmin);
 height = round(ymax - ymin);
 
@@ -80,18 +90,19 @@ height = round(ymax - ymin);
 panorama = zeros([height width 3], 'like', im{1});
 
 blender = vision.AlphaBlender('Operation', 'Binary mask', ...
-	'MaskSource', 'Input port');
-    
+    'MaskSource', 'Input port');
+
 panoramaView = imref2d([height width], [xmin xmax], [ymin ymax]);
 
 for i=1:size(im,1)
     pic = imwarp(im{i}, tform{i}, 'OutputView', panoramaView);
-
+    
     % Generate a binary mask.
-    mask = imwarp(true(size(im{j},1),size(im{j},2)), tform{i}, 'OutputView', panoramaView);
-
+    mask = imwarp(true(size(im{i},1),size(im{i},2)), tform{i}, 'OutputView', panoramaView);
+    
     % Overlay the warpedImage onto the panorama.
     panorama = step(blender, panorama, pic, mask);
+    imshow(panorama)
 end
 
 mosaic = panorama;
